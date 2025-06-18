d1a05b88c79c86212fabccb24b34bc9e6ddf2e6b
cliff keabetswe <innocliffkeab@gmail.com>
2025-06-18 15:46:09 +0200

    tailwindcss installation

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+// This file is used to configure PostCSS, which is a tool for transforming CSS with JavaScript plugins.
+// The `tailwindcss` plugin is used to process Tailwind CSS styles, and the `autoprefixer` plugin adds vendor prefixes to CSS rules for better browser compatibility.
